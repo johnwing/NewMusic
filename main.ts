460eecbb78084b9b3a328da2c5ec712542ae72e8
johnwing <47427803+johnwing@users.noreply.github.com>
2019-12-13 21:38:44 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,23 +6,29 @@
 
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
 namespace tools {
-
+    let beatsPerMinute: number = 120;
+     //% whenUsed
+     const freqs = hex`
+        1f00210023002500270029002c002e003100340037003a003e004100450049004e00520057005c00620068006e00
+        75007b0083008b0093009c00a500af00b900c400d000dc00e900f70006011501260137014a015d01720188019f01
+        b801d201ee010b022a024b026e029302ba02e40210033f037003a403dc03170455049704dd0427057505c8052006
+        7d06e0064907b8072d08a9082d09b9094d0aea0a900b400cfa0cc00d910e6f0f5a1053115b1272139a14d4152017
+        8018f519801b231dde1e`;
+    let _playTone: (frequency: number, duration: number) => void;
+    const MICROBIT_MELODY_ID = 2000;
 
     /**
      * Plays a tone through pin ``P0`` for the given duration.
      * @param frequency pitch of the tone to play in Hertz (Hz), eg: Note.C
      * @param ms tone duration in milliseconds (ms)
      */
-    //% help=music/play-tone weight=90
+    //% weight=90
     //% blockId=device_play_note block="play|tone %note=device_note|for %duration=device_beat" blockGap=8
     //% parts="headphone"
     //% useEnumVal=1
     export function playTone(frequency: number, ms: number): void {
-        pins.analogPitch(frequency, ms);
+        else pins.analogPitch(frequency, ms);
     }
-
-
-
 
     /**
     * 計算長方形面積，並回傳

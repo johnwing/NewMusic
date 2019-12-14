7f4c9deb02c45082c487f66565038d9d2be90ccc
johnwing <47427803+johnwing@users.noreply.github.com>
2019-12-15 01:35:04 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,20 @@
 //% weight=0 color=#000000 icon="\uf0ad" block="Aist2010"
 namespace aist2010 {
+
+    /**
+     * Transport needs to be set prior to using MIDI APIs
+     */
+    let transport: (data: Buffer) => void;
+
+    /**
+     * Sets the transport mechanism to send MIDI commands
+     * @param newTransport current transport
+     */
+    //% advanced=true blockHidden=1
+    export function setTransport(newTransport: (data: Buffer) => void) {
+        transport = newTransport;
+    }
+
     /**
     * 計算長方形面積，並回傳
     */
@@ -16,4 +31,28 @@ namespace aist2010 {
     export function ledOfRectangle(length: number, width:number): void {
         basic.showNumber((length+width)*2)
     }
+
+    /**
+     * Send human readable MIDI messages via serial
+     */
+    //% blockId=midi_serial_transport block="midi use serial"
+    //% weight=1 blockGap=8
+    //% group=Transports
+    export function useSerial() {
+        function send(data: Buffer): void {
+            // waiting for beta
+            //const buf = pins.createBuffer(data.length);
+            //for (let i = 0; i < data.length; ++i)
+            //    buf[i] = data[i];
+            // serial.writeBuffer(buf);
+            serial.writeString("midi:")
+            for (let i = 0; i < data.length; ++i) {
+                if (i > 0) serial.writeString(",");
+                serial.writeNumber(data[i]);
+            }
+            serial.writeLine("");
+        }
+        setTransport(send)
+    }
+
 }
